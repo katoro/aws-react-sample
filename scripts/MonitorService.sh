f1541a4e2a19f6887d2c6b0ef89c8b42899b225e
katoro <you@example.com>
2023-02-22 23:01:00 +0000

    validate

--- scripts/MonitorService.sh
+++ scripts/MonitorService.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo The ValidateService deployment lifecycle event successfully completed. > validate-service.txt
+echo '======================='
+echo 'Running validate_server'
+echo '======================='
+
+result=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:3000/)
+
+echo Check http://localhost:3000/
+echo $result
+
+if [[ "$result" =~ "200" ]]; then
+  exit 0
+else
+  exit 1
+fi
